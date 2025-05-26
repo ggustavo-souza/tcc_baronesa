033e69018995f3255ab03948f9e06008879c4739
= <=>
2025-05-26 11:22:41 -0300

    commit 1 da silva

---
+++ api/bdd.sql
@@ -0,0 +1,35 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+CREATE DATABASE `baronesa_bd`;
+USE baronesa_bd;
+
+CREATE TABLE `moveis` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(60) NOT NULL,
+  `valor` int(6) NOT NULL,
+  `descricao` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(15) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `senha` varchar(50) NOT NULL,
+  `foto` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `foto`) VALUES
+(1, 'Admin', 'cavalogames1231@gmail.com', '2aAnwG7BO/.7I', 'admin.jpg');
+
+
+ALTER TABLE `moveis`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
