f0b4362b99133b2bfd77c4ffd812a64f74dd2183
= <=>
2025-05-30 09:35:15 -0300

    Navbar quase acabada

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,23 +6,24 @@ import '../App.css';
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg CorNavbar">
-            <Link className="navbar-brand" to="/">Baronesa</Link>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/">Início</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/sobre">Sobre</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/contato">Contato</Link>
-                    </li>
-                </ul>
+            <div className="container">
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
+                    <ul className="navbar-nav subir">
+                        <li className="nav-item">
+                            <Link className="nav-link fw-bold fs-5" to="/" style={{ color: '#FFD230' }}>Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link fw-bold fs-5 " to="/sobre" style={{ color: '#FFD230' }}>Sobre Nós</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link fw-bold fs-5 " to="/login" style={{ color: '#FFD230' }}>Login</Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     );
