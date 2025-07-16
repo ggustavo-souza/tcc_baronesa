555c776470b9f98fa26bb041d0fa571f74c769d4
= <=>
2025-07-15 22:49:52 -0300

    comecando orcamento

--- src/components/Orcamento.js
+++ src/components/Orcamento.js
@@ -1,11 +1,21 @@
 import "../App.css";
 import "../awesome/all.min.css";
-import Navbar from "./Navbar";  
+import Navbar from "./Navbar"; 
+import Footer from "./Footer";
 
 function HomeOrcamento() {
     return (
         <main>
             <Navbar />
+            <div className="container mt-5">
+                <div>
+                    <div className="card text-center CardOrcamento mb-5">
+                        <h1 className="mt-5" style={{ color: '#FFD230' }}>Faça já seu orçamento!</h1>
+                        
+                    </div>
+                </div>
+            </div>
+            <Footer />
         </main>
     )
 }
