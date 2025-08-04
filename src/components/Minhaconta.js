d4ca78c01d592e5d13f1f4109db3e491f961ca1c
= <=>
2025-08-04 09:33:20 -0300

    mexeno na minha conta gueedes

--- src/components/Minhaconta.js
+++ src/components/Minhaconta.js
@@ -11,11 +11,28 @@ import { useAuthUser } from "./auths/useAuthUser";
 function MinhaConta() {
     useAuthUser();
 
+    const [NomeUsuario, setNomeUsuario] = useState(null); 
+
+    useEffect(() => {
+        const usuario = JSON.parse(localStorage.getItem("usuarioLogado"));
+        if(usuario) {
+            console.log(usuario.nome);
+            setNomeUsuario(usuario);
+        }
+    }, [])
+
+
     return (
         <main>
             <Navbar />
-            <div>
-                <h1>teste guedes</h1>
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="card mt-4 col-md-10" style={{ backgroundColor: '#503325c1', borderRadius: '10px' }}>
+                        <div>
+                            <h1 style={{ color: '#FFD230' }}>{NomeUsuario ? `Bem vindo ${NomeUsuario.nome}!` : "Carregando..."}</h1>
+                        </div>
+                    </div>  
+                </div>
             </div>
         </main>
     )
