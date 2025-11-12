e7a9165ebb707c0cf9b9d6fba543c5d25484c227
KickDown <oliveiradacruzcaio@gmail.com>
2025-11-12 14:15:54 -0300

    ajustes finos

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-// Assumindo que este caminho agora está correto no seu projeto:
+// O CSS do mapa e os estilos globais (corFooter) permanecem importados
 import '../App.css'; 
 import '../awesome/all.min.css'
 
@@ -9,17 +9,16 @@ function Footer() {
     
     return (
         <div className="container-fluid corFooter">
-            {/* py-4 para um padding vertical, text-lg-start para alinhar texto à esquerda no desktop */}
             <footer className="text-center text-lg-start py-4"> 
                 
                 <div className="container">
-                    {/* row para a grade, align-items-center para verticalizar os itens */}
+                    {/* d-flex align-items-center: Alinha todos os itens verticalmente ao centro para melhor estética */}
                     <div className="row d-flex align-items-center"> 
                         
-                        {/* 1. COLUNA ESQUERDA: REDES SOCIAIS (col-lg-4) */}
+                        {/* 1. COLUNA ESQUERDA: REDES SOCIAIS */}
                         <div className="col-lg-4 col-md-12 mb-4 mb-lg-0 text-white">
-                            <h5 className="mb-3">Siga-nos</h5>
-                            {/* justify-content-lg-start: alinha à esquerda no desktop */}
+                            {/* Ajuste de tamanho de fonte para o título (fs-5 do Bootstrap) */}
+                            <h5 className="mb-3 fs-5">Siga-nos</h5>
                             <div className="d-flex justify-content-center justify-content-lg-start"> 
                                 <a type="button" className="btn btn-warning btn-floating mx-1 footerBotao" href="https://www.facebook.com/profile.php?id=100088414481184" target="blank" aria-label="Facebook">
                                     <i className="fab fa-facebook-f"></i>
@@ -33,28 +32,35 @@ function Footer() {
                             </div>
                         </div>
                         
-                        {/* 2. COLUNA CENTRAL: COPYRIGHT (col-lg-4) */}
+                        {/* 2. COLUNA CENTRAL: COPYRIGHT */}
                         <div className="col-lg-4 col-md-12 mb-4 mb-lg-0 text-center text-white">
-                            <p>© 2025 - 2025 Copyright</p>
-                            <a className="text-white" href="https://abaronesa.vercel.app/"> A Baronesa - Movelaria</a>
+                            {/* Ajustando o tamanho do texto do Copyright via inline style */}
+                            <p style={{ fontSize: '1rem', marginBottom: '0.25rem' }}>© 2025 - 2025 Copyright</p>
+                            <a className="text-white" href="https://abaronesa.vercel.app/" style={{ fontSize: '1rem' }}> 
+                                A Baronesa - Movelaria
+                            </a>
                         </div>
 
-                        {/* 3. COLUNA DIREITA: MAPA (col-lg-4) */}
+                        {/* 3. COLUNA DIREITA: MAPA */}
                         <div className="col-lg-4 col-md-12 text-center text-lg-end"> 
-                             <h5 className="mb-2 text-white text-center text-lg-end">Nossa Localização</h5>
-                             
-                             {/* map-responsive é a chave para o tamanho e responsividade */}
-                             <div className="map-responsive shadow-lg rounded mx-auto mx-lg-0">
-                                <iframe 
-                                    src={mapIframeSrc} 
-                                    width="100%" 
-                                    height="180" 
-                                    style={{ border: 0 }} 
-                                    allowFullScreen="" 
-                                    loading="lazy" 
-                                    referrerPolicy="no-referrer-when-downgrade"
-                                    title="Localização da Empresa no Google Maps"
-                                ></iframe>
+                            {/* Container Flexbox para alinhar o título e o mapa no final da coluna */}
+                            <div className="d-flex flex-column align-items-center align-items-lg-end"> 
+                                {/* Ajuste de tamanho de fonte para o título do mapa (fs-5 do Bootstrap) */}
+                                <h5 className="mb-2 text-white text-center text-lg-end fs-5">Nossa Localização</h5>
+                                 
+                                 {/* O mapa em si. mx-auto (centraliza no mobile), mx-lg-0 (alinha à direita no desktop) */}
+                                 <div className="map-responsive shadow-lg rounded mx-auto mx-lg-0">
+                                    <iframe 
+                                        src={mapIframeSrc} 
+                                        width="100%" 
+                                        height="180" 
+                                        style={{ border: 0 }} 
+                                        allowFullScreen="" 
+                                        loading="lazy" 
+                                        referrerPolicy="no-referrer-when-downgrade"
+                                        title="Localização da Empresa no Google Maps"
+                                    ></iframe>
+                                </div>
                             </div>
                         </div>
                         
