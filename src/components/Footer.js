68eb15ebc5f92eea2dd2774c99db926573f99b77
KickDown <oliveiradacruzcaio@gmail.com>
2025-11-11 21:39:43 -0300

    Links pq o fogaça reclamou lixo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,25 +1,29 @@
-import React from "react";
+import React, { use } from "react";
+import { useNavigate } from "react-router-dom";
 import '../App.css';
 import '../awesome/all.min.css'
 
 function Footer() {
+
+    const navigate = useNavigate();
+
     return (
         <div class="container-fluid corFooter">
             <footer class="text-center text-lg-start">
                 <div class="container-fluid d-flex justify-content-center py-5">
-                    <button type="button" class="btn btn-warning btn-lg btn-floating mx-2 footerBotao">
+                    <button type="button" class="btn btn-warning btn-lg btn-floating mx-2 footerBotao" onClick={navigate("https://www.facebook.com/profile.php?id=100088414481184")}>
                         <i class="fab fa-facebook-f"></i>
                     </button>
-                    <button type="button" class="btn btn-warning btn-lg btn-floating mx-2 footerBotao">
+                    <button type="button" class="btn btn-warning btn-lg btn-floating mx-2 footerBotao" onClick={navigate("wa.me/5515997396140")}>
                         <i class="fab fa-whatsapp"></i>
                     </button>
-                    <button type="button" class="btn btn-warning btn-lg btn-floating mx-2 footerBotao">
+                    <button type="button" class="btn btn-warning btn-lg btn-floating mx-2 footerBotao" onClick={navigate("https://www.instagram.com/abaronesamovelaria/")}>
                         <i class="fab fa-instagram"></i>
                     </button>
                 </div>
                 <div class="text-center text-white p-3">
                     <p>© 2025 - 2025 Copyright</p>
-                    <a class="text-white" href="https://www.abaronesa.com/"> A Baronesa - Movelaria</a>
+                    <a class="text-white" href="https://www.abaronesa.vercel.app/"> A Baronesa - Movelaria</a>
                 </div>
             </footer>
         </div>
